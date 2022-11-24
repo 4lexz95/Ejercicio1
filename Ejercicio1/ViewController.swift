819f34339482750ae97621147d23bda62e4404c4
alex <alejandro.franchzapata@cieep.com>
2022-11-24 17:48:51 +0100

    Initial Commit

---
+++ Ejercicio1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio1
+//
+//  Created by Alex Franch on 24/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
